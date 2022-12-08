ea71426b6ac1bc89b7ce097d998bad52b8adb287
Marc Owen <marcowen20@gmail.com>
2022-12-09 02:25:34 +0900

    Nightly

--- Makefile
+++
@@ -1,6 +0,0 @@
-all:
-	ln -sf "$(HOME)/dotfiles/init.vim" "$(HOME)/.config/nvim/init.vim"
-	ln -sf "$(HOME)/dotfiles/.vimrc" "$(HOME)/.vimrc"
-	ln -sf "$(HOME)/dotfiles/.zshrc" "$(HOME)/.zshrc"
-	ln -sf "$(HOME)/dotfiles/.tmux.conf" "$(HOME)/.tmux.conf"
-	ln -sf "$(HOME)/dotfiles/tmuxinator.yml" "$(HOME)/.config/tmuxinator/tmuxinator.yml"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ln -sf "~/dotfiles/init.vim" "~/.config/nvim/init.vim"
-ln -sf "~/dotfiles/.vimrc" "~/.vimrc"
-ln -sf "~/dotfiles/.zshrc" "~/.zshrc"
-ln -sf "~/dotfiles/.tmux.conf" "~/.tmux.conf"
+# TODO: Change this to makefile, use make install
+# TODO: Convert into full setup script that also installs the packages and plugins, not just config files
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/tmuxinator
+
+ln -sf "$HOME/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/dotfiles/tmuxinator.yml" "$HOME/.config/tmuxinator/tmuxinator.yml"
