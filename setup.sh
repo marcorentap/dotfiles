6aa39817d59a22cfa0eaf34c16cc799e13619c9b
Marc Owen <marcowen20@gmail.com>
2022-12-09 21:46:26 +0900

    Added bat

--- setup.sh
+++ setup.sh
@@ -3,20 +3,21 @@
 #<Install packages>
 sudo apt install git -y
 
+sudo apt install nodejs -y #For CoC
 sudo apt install neovim -y
+sudo apt install bat -y
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt install zsh -y
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt install tmuxinator -y
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+sudo apt install fzf -y
 #</Install packages>
 
 # Config files
