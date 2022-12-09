f254281799a577f1f1b33df2f9281c617cd4a263
Marc Owen <marcowen20@gmail.com>
2022-12-09 21:51:06 +0900

    Added zsh plugins

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
