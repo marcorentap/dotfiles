2bb87afb6e2ef884e3a11fc1e4a89ec0d899424e
Marc Owen <marcowen20@gmail.com>
2022-12-09 21:27:50 +0900

    Testing first automation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# TODO: Change this to makefile, use make install
-# TODO: Convert into full setup script that also installs the packages and plugins, not just config files
+#<Install packages>
+sudo apt install git -y
+
+sudo apt install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo apt isntall zsh -y
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+sudo apt install tmux -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install tmuxinator -y
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+#</Install packages>
+
+# Config files
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/tmuxinator
 
