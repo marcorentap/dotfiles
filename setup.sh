f3b87e1a1dfe33339c2ea5e799ee779f9fb87684
Marc Owen <marcowen20@gmail.com>
2022-12-13 22:23:36 +0900

    Added nvm to path before installing

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ sudo apt install command-not-found -y
 #curl -fsSL https://tailscale.com/install.sh | sh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 
 sudo apt install neovim -y
